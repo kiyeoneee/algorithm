356a218d9de56e5ecdcbce3fbecc394d3eb8092b
kiyeon <kyeoneee@gmail.com>
2020-05-22 19:17:41 +0900

    [20200522] 프로그래머스 연습문제 : dfs/bfs - 단어 변환

---
+++ src/programmers/programmingpractice/dfsbfs/WordConversion.java
@@ -0,0 +1,50 @@
+package programmers.programmingpractice.dfsbfs;
+
+public class WordConversion {
+    // https://programmers.co.kr/learn/courses/30/lessons/43163
+    public int solution(String begin, String target, String[] words) {
+        int answer = 0;
+        boolean isPossible = false;
+
+        for (String word : words) {
+            if (word.equals(target)) {
+                boolean[] isChanged = new boolean[words.length];
+                return change(begin, target, words, isChanged);
+            }
+        }
+
+        return answer;
+    }
+
+    private int change(String begin, String target, String[] words, boolean[] isChecked) {
+        int minCnt = 0;
+
+        for (int i = 0; i < words.length; i++) {
+            if (!isChecked[i] && isChangable(begin, words[i])) {
+                if (words[i].equals(target))
+                    return 1;
+
+                isChecked[i] = true;
+                int cnt = change(words[i], target, words, isChecked) + 1;
+                minCnt = (minCnt == 0 || minCnt > cnt) ? cnt : minCnt;
+                isChecked[i] = false;
+            }
+        }
+
+        return minCnt;
+    }
+
+    private boolean isChangable(String w1, String w2) {
+        boolean isCharDifferent = false;
+        for (int i = 0; i < w1.length(); i++) {
+            if (w1.charAt(i) != w2.charAt(i)) {
+                if (isCharDifferent)
+                    return false;
+                isCharDifferent = true;
+            }
+        }
+
+        return isCharDifferent;
+    }
+}
+
