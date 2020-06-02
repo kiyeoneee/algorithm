a4f8a2064e0178363ba247551d643b192e41a016
kiyeon <kyeoneee@gmail.com>
2020-06-02 23:32:06 +0900

    [20200602] Leetcode : 704. Binary Search

---
+++ src/leetcode/search/BinarySearch.java
@@ -0,0 +1,22 @@
+package leetcode.search;
+
+public class BinarySearch {
+    // https://leetcode.com/problems/binary-search/
+    public int search(int[] nums, int target) {
+        int sIdx = 0, eIdx = nums.length - 1, idx = (sIdx + eIdx)/2;
+
+        while (sIdx <= eIdx) {
+            if (nums[idx] == target) {
+                return idx;
+            } else if (nums[idx] < target) {
+                sIdx = idx + 1;
+            } else {
+                eIdx = idx - 1;
+            }
+
+            idx = (sIdx + eIdx)/2;
+        }
+
+        return -1;
+    }
+}
