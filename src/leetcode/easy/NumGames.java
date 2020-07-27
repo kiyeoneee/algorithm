933f0600b1b30aec594379bba10180c091f2bb83
kiyeon <kyeoneee@gmail.com>
2020-07-27 21:58:21 +0900

    [20200727] Leetcode : 292. Nim Game

---
+++ src/leetcode/easy/NumGames.java
@@ -0,0 +1,9 @@
+package leetcode.easy;
+
+public class NumGames {
+    // url : https://leetcode.com/problems/nim-game/
+
+    public boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
+}
