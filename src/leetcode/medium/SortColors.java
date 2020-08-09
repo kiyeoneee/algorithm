29300c177bbc01ac1cf659cd8cae587e510e3ff8
kiyeoneee <kyeoneee@gmail.com>
2020-08-09 23:35:21 +0900

    [20200808] Leetcode : 75. Sort Colors (2회차)

--- src/leetcode/medium/SortColors.java
+++ src/leetcode/medium/SortColors.java
@@ -3,6 +3,7 @@ package leetcode.medium;
 public class SortColors {
     // url : https://leetcode.com/problems/sort-colors/
 
+    // TODO: 2020/08/09 안 보고 한번 더 풀어보기 
     public void sortColors(int[] nums) {
         int tmp, len = nums.length;
 
@@ -13,6 +14,25 @@ public class SortColors {
                     nums[j] = nums [j + 1];
                     nums[j + 1] = tmp;
                 }
+            }
+        }
+    }
+
+    public void sortColors2(int[] nums) {
+        int start = 0, end = nums.length - 1, index = 0;
+
+        while(index <= end && start < end) {
+            if (nums[index] == 0) {
+                nums[index] = nums[start];
+                nums[start] = 0;
+                ++start;
+                ++index;
+            } else if (nums[index] == 2) {
+                nums[index] = nums[end];
+                nums[end] = 2;
+                --end;
+            } else {
+                ++index;
             }
         }
     }
