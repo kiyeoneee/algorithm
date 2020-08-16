418f5c719bf44e9485b688aeaa6c1a7a712ef1b5
kiyeon <kyeoneee@gmail.com>
2020-08-16 23:39:53 +0900

    [20200816] Leetcode : 328. Odd Even Linked List - refactoring

--- src/leetcode/medium/OddEvenLinkedList.java
+++ src/leetcode/medium/OddEvenLinkedList.java
@@ -3,6 +3,24 @@ package leetcode.medium;
 public class OddEvenLinkedList {
     // url : https://leetcode.com/problems/odd-even-linked-list/
 
+    public ListNode secondOddEvenList(ListNode head) {
+        if (head == null)
+            return null;
+
+        ListNode odd = head, even = head.next, evenHead = even;
+
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+
+        }
+        odd.next = evenHead;
+
+        return head;
+    }
+
     public ListNode oddEvenList(ListNode head) {
         ListNode odd = new ListNode();
         ListNode even = new ListNode();
