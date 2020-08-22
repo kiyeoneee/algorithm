9bef912536b269550f5c5cb1194481456d868cb4
kiyeon <kyeoneee@gmail.com>
2020-08-22 15:52:51 +0900

    [20200822] Leetcode : 88. Merge Sorted Array

---
+++ src/leetcode/easy/MergeSortedArray.java
@@ -0,0 +1,15 @@
+package leetcode.easy;
+
+import java.util.Arrays;
+
+public class MergeSortedArray {
+    // url : https://leetcode.com/problems/merge-sorted-array/
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for (int i = m, j = 0; i < nums1.length; i++, j++) {
+            nums1[i] = nums2[j];
+        }
+
+        Arrays.sort(nums1);
+    }
+}
