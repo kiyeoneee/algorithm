2fdc3929b6280b9472bad349fc15a217a322acef
kiyeon <kyeoneee@gmail.com>
2020-09-09 20:58:41 +0900

    [20200909] Leetcode : 121. Best Time to Buy and Sell Stock - refactoring

--- src/leetcode/easy/BestTimeToBuyAndSellStock.java
+++ src/leetcode/easy/BestTimeToBuyAndSellStock.java
@@ -3,6 +3,21 @@ package leetcode.easy;
 public class BestTimeToBuyAndSellStock {
     // url : https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 
+    public int maxProfitRefactor(int[] prices) {
+        int maxResult = 0, minStock = Integer.MAX_VALUE;
+
+        int length = prices.length;
+        for (int i = 0; i < length; i++) {
+            if (prices[i] < minStock) {
+                minStock = prices[i];
+            } else {
+                maxResult = Math.max(prices[i] - minStock, maxResult);
+            }
+        }
+
+        return maxResult;
+    }
+
     public int maxProfit(int[] prices) {
         int maxResult = 0, benefit = 0;
 
