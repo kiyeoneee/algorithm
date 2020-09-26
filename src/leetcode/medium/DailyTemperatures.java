5a1f222fc82b5591d4d23bfbe087c0fd3e3e19db
kiyeoneee <kyeoneee@gmail.com>
2020-09-26 18:22:14 +0900

    [20200926] Leetcode : 739. Daily Temperatures - 2nd

--- src/leetcode/medium/DailyTemperatures.java
+++ src/leetcode/medium/DailyTemperatures.java
@@ -1,6 +1,9 @@
 package leetcode.medium;
 
+import java.util.Stack;
+
 public class DailyTemperatures {
+    // TODO: 2020/09/26 Brute force 이외의 방법은..?
 
     /**
      * url : https://leetcode.com/problems/daily-temperatures/
@@ -10,7 +13,7 @@ public class DailyTemperatures {
      * Runtime : faster than 6.22% of Java online submissions
      * Memory Usage : less than 75.51% of Java online submissions
      * Time complexity : O(n^2)
-     * Space complexity : O(1)
+     * Space complexity : O(w)
      */
     public int[] dailyTemperatures(int[] T) {
         int[] result = new int[T.length];
@@ -22,6 +25,30 @@ public class DailyTemperatures {
                     break;
                 }
             }
+        }
+
+        return result;
+    }
+
+    /**
+     * Stack
+     * 이걸 스택으로...? 서브 미션 보고 푼 방법
+     *
+     * Runtime : faster than 52.85% of Java online submissions
+     * Memory Usage : less than 79.89% of Java online submissions
+     * Time complexity : O(n^2)
+     * Space complexity : O(w)
+     */
+    public int[] dailyTemperaturesStack(int[] T) {
+        int[] result = new int[T.length];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < T.length; i++) {
+            while (!stack.empty() && T[stack.peek()] < T[i]) {
+                result[stack.peek()] = i - stack.peek();
+                stack.pop();
+            }
+            stack.push(i);
         }
 
         return result;
